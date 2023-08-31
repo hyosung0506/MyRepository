ada7f0bf7c557c614e673c21f2d27e3060b9765c
Sbebe45 <hyosung0506@gmail.com>
2023-08-31 22:46:44 +0900

    first commit

---
+++ src/main/java/com/githubActionTest/projectHS/Controller.java
@@ -0,0 +1,14 @@
+package com.githubActionTest.projectHS;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/'")
+public class Controller {
+    @GetMapping
+    public String hell(){
+        return "Hello ;)"
+    }
+}
---
+++ src/main/java/com/githubActionTest/projectHS/ProjectHsApplication.java
@@ -0,0 +1,13 @@
+package com.githubActionTest.projectHS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHsApplication.class, args);
+	}
+
+}
